525eeb1896b73df2e1e5f9a6c9fdba1e94163b79
Mechouille <65155702+laura-mourrat@users.noreply.github.com>
2025-07-15 16:07:40 +0400

    Mise a jour du code

---
+++ client.js
@@ -0,0 +1,18 @@
+// client.js
+window.TrelloPowerUp.initialize({
+  'card-badges': function (t, options) {
+    return t.card('checklists')
+      .then(function (card) {
+        const items = [];
+        card.checklists.forEach(checklist => {
+          checklist.checkItems.forEach(item => {
+            items.push(item.name);
+          });
+        });
+        return items.slice(0, 3).map(name => ({
+          text: name,
+          color: 'blue'
+        }));
+      });
+  }
+});
